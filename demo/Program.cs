90997ab5d981be514936cc75cd14ff42a0cf833f
darktoom <darktoom789@gmail.com>
2024-12-13 20:44:11 +0200

    Add project files.

---
+++ demo/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region loops
+            #region for-foreach
+            //for
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //for (int i = 0; i < 5; i++) { 
+            //Console.WriteLine(numbers[i]);  
+
+            //}
+            //Console.WriteLine("************************");
+            //foreach
+
+            //foreach (int i in numbers) {
+            //    Console.WriteLine(i);
+            //}
+
+
+            #endregion
+            #region while - do while
+            //do while
+            // int number;
+            //Console.WriteLine("pls, enter even number");
+            //number = int.Parse(Console.ReadLine());
+            //while (!(number % 2 == 0)) {
+            //    Console.WriteLine("pls, enter number even");
+            //number =int.Parse(Console.ReadLine());
+
+            //}
+            //bool flag;
+            //do
+            //{
+            //    Console.WriteLine("enter even number");
+            //    // number=int.TryParse(Console.ReadLine(),out );
+            //    flag = int.TryParse(Console.ReadLine(), out number);
+            //}while (!(number % 2 == 0)||  ! flag== true );
+
+            // while
+            int x=4;
+            while (x % 2 == 0)
+            {
+                Console.WriteLine("even number");
+                Console.WriteLine("enter odd num");
+                x=int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("odd number");
+            #endregion
+
+            
+
+            #endregion
+        }
+    }
+}
